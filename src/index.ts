f6d86217c90ee9444ea5ce1a17739c21de902620
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-12-12 15:50:23 +0545

    feat: add key handles

--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,31 @@ import os from "node:os";
 import { DOWNLOAD_DIR } from "./constant";
 import { IsNetworkAvailable, downloadAndExtractFonts, executeCommand, selectFonts } from "./util";
 
+
+process.stdin.on('keypress', (_char, key) => {
+  if (key.ctrl && key.name === 'c') {
+    process.exit(0);
+  }
+})
+
 export async function main() {
   await IsNetworkAvailable();
   const selectedFonts = await selectFonts();
 
-  if (selectedFonts.length === 0) {
-    console.info("❌ No fonts selected. Exiting...");
-  }
-  else {
-    // Ensure the download directory exists
-    if (!fs.existsSync(DOWNLOAD_DIR))
-      fs.mkdirSync(DOWNLOAD_DIR);
 
-    await downloadAndExtractFonts(selectedFonts);
+  // Ensure the download directory exists
+  if (!fs.existsSync(DOWNLOAD_DIR))
+    fs.mkdirSync(DOWNLOAD_DIR);
 
-    // Update font cache and rebuild font cache on Linux and macOS systems
-    if (os.platform() !== "win32" || os.platform() !== "android")
-      executeCommand("fc-cache -f -v");
-  }
+  await downloadAndExtractFonts(selectedFonts);
+
+  // Update font cache and rebuild font cache on Linux and macOS systems
+  if (os.platform() !== "win32" || os.platform() !== "android")
+    executeCommand("fc-cache -f -v");
+
 }
 
 main().catch((error) => {
-  console.error("❌ Error:", error);
+  console.log(error);
   process.exit(1);
 });
